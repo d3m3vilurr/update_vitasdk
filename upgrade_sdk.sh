e9edfc4909c8be41c47e0da90aae7d9347c9895d
Sunguk Lee <d3m3vilurr@gmail.com>
2016-12-18 03:09:35 +0900

    Change script for vita-makepkg & new vdpm

--- upgrade_sdk.sh
+++ upgrade_sdk.sh
@@ -1,23 +1,33 @@
 #!/usr/bin/env bash
+#install build toolchain
 mkdir -p $VITASDK
 mv $VITASDK $VITASDK.bak
 curl -s https://api.github.com/repos/vitasdk/autobuilds/releases | grep browser_download_url | grep linux | head -n 1 | cut -d '"' -f 4 | xargs curl -L | tar xvj
 mv vitasdk $VITASDK
 #cp -r vitasdk $VITASDK
 
-mkdir build
-mkdir patches
+# install vdpm
+#curl -sL https://github.com/vitasdk/vdpm/raw/master/vdpm -o $VITASDK/bin/vdpm
+curl -sL https://github.com/d3m3vilurr/vdpm/raw/support-local-xz/vdpm -o vdpm
+chmod +x vdpm
+cp vdpm $VITASDK/bin/vdpm
 
-curl -sL https://github.com/ooPo/vitatoolchain/raw/master/patches/expat-25c6393829.patch -o patches/expat-25c6393829.patch
-curl -sL https://github.com/ooPo/vitatoolchain/raw/master/patches/opus-update-ssl-curl.patch -o patches/opus-update-ssl-curl.patch
-
-cd build
-curl -sL https://github.com/ooPo/vitatoolchain/raw/master/scripts/007-vdpm.sh | bash
-curl -sL https://github.com/ooPo/vitatoolchain/raw/master/scripts/009-libftpvita.sh | bash
-curl -sL https://github.com/ooPo/vitatoolchain/raw/master/scripts/010-vita-openssl.sh | bash
-curl -sL https://github.com/ooPo/vitatoolchain/raw/master/scripts/011-vita-curl.sh | bash
-curl -sL https://github.com/ooPo/vitatoolchain/raw/master/scripts/012-expat.sh | bash
-curl -sL https://github.com/ooPo/vitatoolchain/raw/master/scripts/013-opus.sh | bash
+# install makepkg
+#git clone https://github.com/vitasdk/vita-makepkg.git
+git clone https://github.com/d3m3vilurr/vita-makepkg.git
+cd vita-makepkg
+git checkout fix-hardcoded-path
+cp makepkg.conf.sample makepkg.conf
 cd ..
-rm -rf build patches
+mv vita-makepkg $VITASDK/lib
+ln -s $VITASDK/lib/vita-makepkg/vita-makepkg $VITASDK/bin/vita-makepkg
+
+# install vita packages
+git clone https://github.com/vitasdk/packages.git
+mv packages $VITASDK/
+
+# install all vdpm packages
+curl -L https://github.com/vitasdk/vdpm/raw/master/install-all.sh | bash
+
+rm vdpm
 rm -rf $VITASDK.bak
